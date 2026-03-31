753d02ff84f5c09dcbaa2223df2e6cd88e3f71ec
sofiatrade <lindbergbeatrice85@gmail.com>
2026-03-31 08:20:42 +0200

    Update fetch-csv.js

--- netlify/functions/fetch-csv.js
+++ netlify/functions/fetch-csv.js
@@ -8,7 +8,17 @@ exports.handler = async function(event) {
   try {
     const text = await new Promise((resolve, reject) => {
       const lib = url.startsWith('https') ? https : http;
-      lib.get(url, (res) => {
+      const options = {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          'Accept-Language': 'en-US,en;q=0.5',
+          'Accept-Encoding': 'identity',
+          'Connection': 'keep-alive',
+          'Referer': 'https://marketcharts.com/',
+        }
+      };
+      lib.get(url, options, (res) => {
         let data = '';
         res.on('data', chunk => data += chunk);
         res.on('end', () => resolve(data));
